1038da7a6ff237618342ba3afadf6c5a46846720
siavava <amittaijoel@outlook.com>
2024-01-30 16:17:45 -0500

    add base structure, some example routes

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# project-short-learning-recommendation-engine
-Recommendation Engine work for ShortForm Learning
+# Discite RE Engine
+
+## Introduction
+
+This repo implements a simple recommendation engine for the Discite project.
+
+### Tech Stack
+
+| Technology | Why |
+| ---------- | --- |
+| [Python][python] | Python is easy to use. |
+| [FastAPI][fast-api] | For developing quick-iteration API's to interact with out engine. |
+| [Pinecone][pinecone] | For vector similarity search. |
+
+### Architecture
+
+&copy; 2023 [Discite][discite]
+
+[discite]: https://discite.tech
+[python]: https://www.python.org/
+[fast-api]: https://fastapi.tiangolo.com/
+[pinecone]: https://www.pinecone.io/
---
+++ main.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    Main entrypoint into our recommentation engine.
+"""
+
+from fastapi import FastAPI
+from pinecone import Pinecone, ServerlessSpec
+
+from dotenv import load_dotenv
+import os
+
+app = FastAPI()
+
+# load .env file
+load_dotenv()
+
+print("PINECONE_API_KEY: ", os.getenv("PINECONE_API_KEY"))
+
+# get PINECONE_API_KEY from .env file
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+
+# Create a Pinecone client
+pc = Pinecone(api_key=PINECONE_API_KEY)
+
+app.get("/")
+def read_root():
+    """
+        Root endpoint.
+    """
+    return {"Hello": "World"}
+
+@app.get("/test-add")
+def test_add():
+    """
+        Test the Pinecone client.
+    """
+    
+    try:
+        index = pc.Index("quickstart")
+    except:
+        pc.create_index(
+            
+            name="quickstart",
+            dimension=8,
+            metric="euclidean",
+            spec=ServerlessSpec(
+                cloud='aws', 
+                region='us-west-2'
+            ) 
+        )
+        
+        index = pc.Index("quickstart")
+
+
+    index.upsert(
+        vectors=[
+            {"id": "vec1", "values": [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]},
+            {"id": "vec2", "values": [0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2]},
+            {"id": "vec3", "values": [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3]},
+            {"id": "vec4", "values": [0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4]}
+        ],
+        namespace="ns1"
+    )
+
+    index.upsert(
+        vectors=[
+            {"id": "vec5", "values": [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]},
+            {"id": "vec6", "values": [0.6, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6]},
+            {"id": "vec7", "values": [0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7]},
+            {"id": "vec8", "values": [0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8]}
+        ],
+        namespace="ns2"
+    )
+    
+    index_stats = index.describe_index_stats()
+    
+    return {"index-stats": index_stats.to_dict()}
+
+@app.get("/test-query")
+def test_query():
+    """
+        Test the Pinecone client.
+    """
+
+    index = pc.Index("quickstart")
+
+    v1 = index.query(
+        namespace="ns1",
+        vector=[0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3],
+        top_k=3,
+        include_values=True
+    )
+
+    v2 = index.query(
+        namespace="ns2",
+        vector=[0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7],
+        top_k=3,
+        include_values=True
+    )
+    
+    return {"v1": v1.to_dict(), "v2": v2.to_dict()}
+
+@app.get("/test-delete")
+def test_delete():
+    """
+        Test the Pinecone client.
+    """
+
+    index = pc.Index("quickstart")
+
+    index.delete(
+        ids=["vec1", "vec2", "vec3", "vec4"],
+        namespace="ns1"
+    ) 
+
+    index.delete(
+        ids=["vec5", "vec6", "vec7", "vec8"],
+        namespace="ns2"
+    )
+    
+    return {"status": "success"}
