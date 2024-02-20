37fe3f954b19bfcedcaa4c1eefd024822e1dff20
siavava <amittaijoel@outlook.com>
2024-02-20 15:52:17 -0500

    update exports

---
+++ search/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    search
+    ~~~~~~
+
+    This module contains search functionality using Algolia.
+"""
+
+from .algoliaclient import AlgoliaSearchClient
+
+__all__ = [
+    "AlgoliaSearchClient"
+    ]
