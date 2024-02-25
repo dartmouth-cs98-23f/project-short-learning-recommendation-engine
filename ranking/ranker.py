0084a196774f4b23b2d2a5478c0579de68484dc7
siavava <amittaijoel@outlook.com>
2024-02-25 16:38:58 -0500

    added fetch logic

--- ranking/ranker.py
+++ ranking/ranker.py
@@ -5,21 +5,35 @@
     Video Ranking Utils
 """
 
+from dotenv import load_dotenv
+import os
+import requests
+
+# import requests
+
+
 class VideoRanker:
     """
         Video ranking class.
     """
-    def __init__(self):
-        """
-            Constructor.
-        """
-        pass
+    def __init__(self, /, backend_url = None, debug = False):
+        
+        load_dotenv()
+
+        self.backend_url = backend_url or os.getenv("BACKEND_URL")
+        self.debug = debug
 
     def get_user_affinities(self, user_id: str):
         """
             Get user affinities.
         """
-        return {}
+        endpoint = f"{self.backend_url}/api/user/affinities/admin"
+        payload = { "user_id": user_id }
+        res = requests.get(endpoint, json=payload)
+        
+        if self.debug:
+            print(f"GET USER AFFINITIES: {user_id} -> {res.json() = }")
+        return res.json()
 
     
     def rank_videos(self, videos, user_id: str):
@@ -40,3 +54,8 @@ class VideoRanker:
         videos.sort(key=lambda x: x.weight)
         
         return videos
+
+if __name__ == "__main__":
+    ranker = VideoRanker()
+    print(ranker.get_user_affinities("user_1"))
+    print(ranker.rank_videos([{"topic": "Python", "score": 0.5}, {"topic": "Python", "score": 0.7}], "user_1"))
