055aa641e9fe240e2338c210e7090c5bce8ef22c
Kevin Lin <linkevin281@gmail.com>
2024-02-26 06:13:23 +0000

    Update for final inference

--- inference/pipeline/inference.py
+++ inference/pipeline/inference.py
@@ -1,12 +1,17 @@
-import sys, os, datetime, dotenv, time
+import sys, os, datetime, dotenv, time, re, json
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from pinecone import Pinecone
 from validator import validate_inference_output
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import string
+import nltk
+from nltk.corpus import stopwords
+from utils.transcript import download_transcript
 
-dotenv.load_dotenv(dotenv_path="../.env")
+dotenv.load_dotenv(dotenv_path="/home/ubuntu/InferenceTest1/rec/inference/.env")
+# print current os location
 
 ########################
 ## GLOBAL VARIABLES ###
@@ -16,29 +21,29 @@ current_doc = None
 #################
 ## CONSTANTS ###
 ################
-OUTPUT_DIR = sys.argv[1]
-DB_NAME = "preTechnigala_db"
+OUTPUT_DIR = "/home/ubuntu/InferenceTest1/rec/inference/pipeline/data"
+DB_NAME = "preTechnigalaClean_db"
 COLLECTION_NAME = "video_metadata"
 MONGO_DB_CLIENT = MongoClient(os.getenv("MONGODB_URI"), server_api=ServerApi('1'))
-PINECONE_INDEX_NAME = "preTechnigala"
-PINECONE_CLIENT = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+PINECONE_INDEX_NAME = "pretechnigala"
+PINECONE_CLIENT = Pinecone(api_key=os.getenv("PINECONE_KEY"))
 
 MAX_INFERENCE_RUNS = 3
 SHOTS = ["mixtral8x7b", "full-stack"]
-SHOTS_CHAR_LIMIT = 6500
-INPUT_CHAR_LIMIT = 6500
+SHOTS_CHAR_LIMIT = 5550
+INPUT_CHAR_LIMIT = 5550
 TEMPERATURE = 0.5
 TOP_K = 35
 TOP_P = 0.96
 
-def main(output_dir):
+def main():
     """
     Main function to run the pipeline. If an error occurs in inference or embedding generation, 
     this subprocess is restarted. If validation fails up to MAX_INFERENCE_RUNS times, 
     or any other error occurs, the document is marked as failed and the next document is processed.
     """
     global current_doc
-
+    start_time = time.time()
     doc = get_next_document()
     if doc is None:
         print("No more documents to process")
@@ -52,24 +57,80 @@ def main(output_dir):
 
     while current_doc:
         try:
+            print(f'\n########################################\n# ID: {current_doc["_id"]} #\n###########################################\n')
+            print(f'Runtime so far: {(time.time() - start_time)/60:.2f} minutes')
+            print(f'Memory: {torch.cuda.memory_allocated()/1024**3:.2f} GB')
+            clean_transcript(current_doc)
             ## Try to run inference and validate output up to MAX_INFERENCE_RUNS times
+            final_path = None
+            did_fail = False
             for run in range(MAX_INFERENCE_RUNS):
                 print(f"Run {run+1}/{MAX_INFERENCE_RUNS}")
-                run_inference(device, tokenizer, model)
+                path = f'{OUTPUT_DIR}/outputs/{current_doc["_id"]}/inference_output_{run}.json'
+                final_path = path
+                run_inference(device, tokenizer, model, path)
                 try:
-                    validate_inference_output(f'{OUTPUT_DIR}/outputs/{current_doc["_id"]}/inference_output.json')
+                    if validate_inference_output(path):
+                        did_fail = False
+                        break
                 except Exception as e:
                     print(f"Validation failed: {e}")
+                    did_fail = True
                     continue
-            generate_embedding(device, tokenizer, model)
-            upload_embeddings()
+            generate_embedding(device, tokenizer, model, path)
+            upload_to_database(did_fail, final_path)
+
+            current_doc = get_next_document()
         except Exception as e:
             ## If an error occurs, mark the document as failed
             collection = MONGO_DB_CLIENT[DB_NAME][COLLECTION_NAME]
             collection.update_one({"_id": current_doc["_id"]}, {"$set": {"vectorizeFailed": True}})
+            print(f"Error processing document: {e}")
+            current_doc = get_next_document()
     
     return 0
 
+def clean_transcript(doc):
+    """
+    Clean the transcript of any unwanted characters
+    Args:
+        doc (dict): The document to clean
+    """
+    yt_id = doc["youtubeURL"].split("v=")[1]
+    name = doc["_id"]
+    print(f"Cleaning transcript yt_id: {yt_id}")
+    os.makedirs(f'{OUTPUT_DIR}/transcripts/raw', exist_ok=True)
+    os.makedirs(f'{OUTPUT_DIR}/transcripts/clean', exist_ok=True)
+    os.makedirs(f'{OUTPUT_DIR}/transcripts/processed', exist_ok=True)
+
+    download_transcript(yt_id, f'{OUTPUT_DIR}/transcripts/raw/{name}.txt')
+    with open(f'{OUTPUT_DIR}/transcripts/raw/{name}.txt', 'r') as file:
+        with open(f'{OUTPUT_DIR}/transcripts/clean/{name}.txt', 'w') as file_clean:
+            for i,line in enumerate(file):
+                if i == 0:
+                    continue
+                ## Remove Timestamp
+                pattern = r'\d+\.\d+\,\d+\.\d+\,'
+                line = re.sub(pattern, '', line)
+                line = line.replace("'", "")
+                line = line.replace('"', "")
+                ## if last char is a space, remove
+                if line[-1] == " ":
+                    line = line[:-1]
+                line = line.replace("  ", " ")
+                line = line.replace('\n', ' ')
+                file_clean.write(line)
+
+    nltk.download('stopwords')
+    with open(f'{OUTPUT_DIR}/transcripts/clean/{name}.txt', 'r') as file:
+        with open(f'{OUTPUT_DIR}/transcripts/processed/{name}.txt', 'w') as file_processed:
+            text = file.read()
+            text = text.lower()
+            text = text.translate(str.maketrans('', '', string.punctuation))
+            stop_words = set(stopwords.words('english'))
+            filtered_text = [word for word in text.split() if word not in stop_words]
+            filtered_text = ' '.join(filtered_text)
+            file_processed.write(text)
 
 def abridge_transcript(transcript: str, chars: int) -> str:
     """ Abridge a transcript to a certain number of characters
@@ -132,7 +193,7 @@ def build_message(shots: list, transcript: str, prompt: str) -> list:
     
     return messages
 
-def run_inference(device, tokenizer, model):
+def run_inference(device, tokenizer, model, path):
     """
     Run inference on the model
     Args:
@@ -144,9 +205,10 @@ def run_inference(device, tokenizer, model):
     """
     try:
         global current_doc
+        os.makedirs(f'{OUTPUT_DIR}/outputs/{current_doc["_id"]}', exist_ok=True)
 
         f_prompt = open(f'{OUTPUT_DIR}/inputs/prompt.txt', 'r', encoding='utf-8')
-        f_transcript = open(f'{OUTPUT_DIR}/outputs/{current_doc["_id"]}/transcript.txt', 'r', encoding='utf-8')
+        f_transcript = open(f'{OUTPUT_DIR}/transcripts/processed/{current_doc["_id"]}.txt', 'r', encoding='utf-8')
         transcript = f_transcript.read()
         prompt = f_prompt.read()
         f_prompt.close()
@@ -167,14 +229,14 @@ def run_inference(device, tokenizer, model):
         del inputs
         torch.cuda.empty_cache()
 
-        f_output = open(f'{OUTPUT_DIR}/outputs/{current_doc["_id"]}/inference_output.json', 'w', encoding='utf-8')
-        f_output.write(decoded[0])
+        f_output = open(path, 'w', encoding='utf-8')
+        f_output.write(decoded[0].split('[/INST]')[-1][1:-4])
         f_output.close()
     except Exception as e:
         print(f"Error running inference: {e}")
         sys.exit(1)
 
-def generate_embedding(device, tokenizer, model):
+def generate_embedding(device, tokenizer, model, summary_path):
     """
     Generate an embedding for the transcript.
     Args:
@@ -189,7 +251,7 @@ def generate_embedding(device, tokenizer, model):
     global current_doc
     try:
 
-        f_summary = open(f'{OUTPUT_DIR}/outputs/{current_doc["_id"]}/inference_output.json', 'r', encoding='utf-8')
+        f_summary = open(summary_path, 'r', encoding='utf-8')
         summary = f_summary.read()
         f_summary.close()
 
@@ -197,7 +259,6 @@ def generate_embedding(device, tokenizer, model):
         inputs = tokenizer(summary, return_tensors="pt").to(device)
         with torch.no_grad():
             hidden = model(**inputs, output_hidden_states=True)
-            hidden_states = hidden.hidden_states
         
         print(f"Embedding generation time taken: {time.time() - start_time:.2f} seconds")
 
@@ -217,29 +278,46 @@ def generate_embedding(device, tokenizer, model):
         print(f"Error generating embedding: {e}")
         sys.exit(1)
     
-def upload_embeddings():
+def upload_to_database(did_fail, final_path):
     """
-    Upload an embedding to Pinecone
-    Args:
-        embedding (torch.Tensor): The embedding to upload
+    Upload an embedding to Pinecone and the document to MongoDB
     """
     global current_doc
-    try:
-        index = PINECONE_CLIENT.Index(PINECONE_INDEX_NAME)
+    ## MongoDB Update
+    collection = MONGO_DB_CLIENT[DB_NAME][COLLECTION_NAME]
+    inference_collection = MONGO_DB_CLIENT[DB_NAME]["inference_summary"]
+    
+    ## upload with topicIds and complexities
+    topics = []
+    complexities = []
+    with open(final_path, 'r') as file:
+        output = file.read()
+        json_summary = json.loads(output)
+        generalTopics = json_summary["generalTopics"]
+        for obj in generalTopics:
+            topics.append(obj["name"])
+            complexities.append(obj["complexity"])
 
-        max_pool = torch.load(f'{OUTPUT_DIR}/outputs/{current_doc["_id"]}/max_pool.pt').to('cpu').numpy().tolist()
-        avg_pool = torch.load(f'{OUTPUT_DIR}/outputs/{current_doc["_id"]}/avg_pool.pt').to('cpu').numpy().tolist()
+    collection.update_one({"_id": current_doc["_id"]}, {"$set": {"isVectorized": True, "inferenceTopics": topics, "inferenceComplexities": complexities, "inferenceMisshape": did_fail, "vectorizeFailed": False}})
+    inference_collection.insert_one({"_id": current_doc["_id"], "inferenceSummary": json_summary})
 
-        metadata_max = {"type": "max_pool", "title": current_doc["title"], "topics": current_doc["topics"], "videoID": current_doc["_id"]}
-        metadata_avg = {"type": "avg_pool", "title": current_doc["title"], "topics": current_doc["topics"], "videoID": current_doc["_id"]}
-        vectors = [
-            {"id": current_doc["_id"], "values": max_pool, "metadata": metadata_max},
-            {"id": current_doc["_id"], "values": avg_pool, "metadata": metadata_avg}
-        ]
-        index.upsert(vectors=vectors)
-        print(f"Embedding uploaded for document: {current_doc['_id']}")
-    except Exception as e:
-        print(f"Error uploading embedding: {e}")
+    print(f"Document updated in MongoDB: {current_doc['_id']} and inference summary uploaded")
+
+    ## Pinecone Upload
+    index = PINECONE_CLIENT.Index(PINECONE_INDEX_NAME)
+
+    max_pool = torch.load(f'{OUTPUT_DIR}/outputs/{current_doc["_id"]}/max_pool.pt').to('cpu').numpy().tolist()
+    avg_pool = torch.load(f'{OUTPUT_DIR}/outputs/{current_doc["_id"]}/avg_pool.pt').to('cpu').numpy().tolist()
+
+    metadata_max = {"type": "max_pool", "title": current_doc["title"], "topics": [str(topic) for topic in current_doc["topicId"]], "videoID": str(current_doc["_id"]), "inferenceTopics": [str(topic) for topic in topics], "inferenceComplexities": [str(complexity) for complexity in complexities]}
+    metadata_avg = {"type": "avg_pool", "title": current_doc["title"], "topics": [str(topic) for topic in current_doc["topicId"]], "videoID": str(current_doc["_id"]), "inferenceTopics": [str(topic) for topic in topics], "inferenceComplexities": [str(complexity) for complexity in complexities]}
+
+    vectors = [
+        {"id": str(current_doc["_id"]), "values": max_pool, "metadata": metadata_max},
+        {"id": str(current_doc["_id"]), "values": avg_pool, "metadata": metadata_avg}
+    ]
+    index.upsert(vectors=vectors)
+    print(f"Embedding uploaded for document: {current_doc['_id']}")
 
 def get_next_document():
     """
